a60ab27c71944ce3b46881e16ffc98c56d9254ef
subaru-hello <octosubaru0926@gmail.com>
2023-03-19 08:14:34 +0900

    image generation

--- prompt-app/src/App.tsx
+++ prompt-app/src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Configuration, OpenAIApi } from 'openai';
 
 function App() {
+  const [prompt, setPrompt] = useState<string>('');
+  const [result, setResult] = useState<any>('');
+  const configuration = new Configuration({
+    organization: process.env.REACT_APP_OPENAI_ORG,
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const response = await openai.createImage({
+      prompt,
+      n: 2,
+      size: '1024x1024',
+    });
+    const image_url = response.data.data[0].url;
+    console.log(image_url);
+    setResult(image_url);
+  };
+
+  const handlePromptChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setPrompt(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <textarea value={prompt} onChange={handlePromptChange} />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {result && <img height={100} width={100} src={result} alt={prompt} />}
+      </div>
     </div>
   );
 }
